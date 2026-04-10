0ba1edb0cc6c0abab5ee919e1ee6e0dd0b45c738
WinterWolfVN <winterwolfvn123@gmail.com>
2026-04-10 21:05:31 +0700

    Update build.gradle.kts

--- patch-loader/build.gradle.kts
+++ patch-loader/build.gradle.kts
@@ -69,7 +69,6 @@ dependencies {
     implementation(projects.services.daemonService)
     implementation(projects.share.android)
     implementation(projects.share.java)
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs_nio:2.1.5")
 
     implementation(libs.gson)
 }
