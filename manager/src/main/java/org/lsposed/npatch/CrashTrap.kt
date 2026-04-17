6ff2fb8b6d392647d89ab4446bbd5f34a1881c74
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-04-18 00:10:26 +0700

    Update CrashTrap.kt

--- manager/src/main/java/org/lsposed/npatch/CrashTrap.kt
+++ manager/src/main/java/org/lsposed/npatch/CrashTrap.kt
@@ -1,32 +1,33 @@
-package org.lsposed.npatch
+package ỏg.lsposed.npatch
 
 import android.content.Context
 import android.os.Build
-import android.util.Log
 import java.io.File
 import java.io.PrintWriter
 import java.io.StringWriter
 
 object CrashTrap {
     @JvmStatic
-    fun start(ctx: Context?) {        
-        val context = ctx ?: return 
-        
+    fun start(ctx: Context?) {
+        val context = ctx ?: return
         val originalHandler = Thread.getDefaultUncaughtExceptionHandler()
+        
         Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
             try {
                 val sw = StringWriter()
                 throwable.printStackTrace(PrintWriter(sw))
                 
-                val report = "--- NPATCH CRASH ---\n" +
-                             "Device: ${Build.MODEL}\n" +
-                             "Android: ${Build.VERSION.RELEASE}\n" +
-                             "Reason: ${throwable.message}\n\n" +
+                val report = "--- NPATCH CRASH REPORT ---\n" +
+                             "Device: " + Build.MODEL + "\n" +
+                             "Android: " + Build.VERSION.RELEASE + "\n" +
+                             "Reason: " + throwable.message + "\n\n" +
                              "--- STACK TRACE ---\n" + sw.toString()
 
-                val logFile = File(context.filesDir, "NPATCH_CRASH.txt")
+                val logDir = context.getExternalFilesDir(null) ?: context.filesDir
+                val logFile = File(logDir, "NPATCH_CRASH.txt")
+                
                 logFile.writeBytes(report.toByteArray())
-            } catch (e: Exception) {            
+            } catch (e: Exception) {
             }
             originalHandler?.uncaughtException(thread, throwable)
         }
