9d4b5c15045915916550d3c8e3960bf4c2844477
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-04-17 20:58:06 +0700

    Update LSPApplication.kt

--- manager/src/main/java/org/lsposed/npatch/LSPApplication.kt
+++ manager/src/main/java/org/lsposed/npatch/LSPApplication.kt
@@ -29,7 +29,7 @@ class LSPApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         // verifySignature()
-        context?.let { CrashTrap.start(it) }
+        CrashTrap.start(context)
         
         try {
         } catch (e: UnsatisfiedLinkError) {
