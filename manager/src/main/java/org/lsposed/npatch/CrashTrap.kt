03fccea1183b75da6b004a51d73eecd20c6f9fac
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-04-22 20:33:36 +0700

    Update CrashTrap.kt

--- manager/src/main/java/org/lsposed/npatch/CrashTrap.kt
+++ manager/src/main/java/org/lsposed/npatch/CrashTrap.kt
@@ -1,50 +1,37 @@
-package org.lsposed.npatch
+package org.lsposed.npatch;
 
-import android.content.Context
-import android.os.Build
-import java.io.File
-import java.io.PrintWriter
-import java.io.StringWriter
+import android.content.Context;
+import android.os.Build;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
-object CrashTrap {
-    @JvmStatic
-    fun start(ctx: Context?) {
-        val context = ctx ?: return
-        val originalHandler = Thread.getDefaultUncaughtExceptionHandler()
-        
-        val prefixes = arrayOf("org.lsposed")
+public class CrashTrap {
+    public static void start(Context ctx) {
+        if (ctx == null) return;
+        final Thread.UncaughtExceptionHandler originalHandler = Thread.getDefaultUncaughtExceptionHandler();
 
-        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
             try {
-                val sw = StringWriter()
-                throwable.printStackTrace(PrintWriter(sw))
-                val stackTrace = throwable.stackTrace
-                
-                var culprit = "None"
-                
-                outer@ for (element in stackTrace) {
-                    val className = element.className
-                    for (prefix in prefixes) {
-                        if (className.startsWith(prefix)) {
-                            culprit = className
-                            break@outer
-                        }
-                    }
-                }
-                
-                val report = "--- NPATCH CRASH REPORT ---\n" +
+                StringWriter sw = new StringWriter();
+                throwable.printStackTrace(new PrintWriter(sw));
+
+                String report = "--- NPATCH CRASH REPORT ---\n" +
                              "Device: " + Build.MODEL + "\n" +
                              "Android: " + Build.VERSION.RELEASE + "\n" +
-                             "Culprit Class: " + culprit + "\n" +
-                             "Reason: " + throwable.message + "\n\n" +
-                             "--- STACK TRACE ---\n" + sw.toString()
+                             "Reason: " + throwable.getMessage() + "\n\n" +
+                             "--- STACK TRACE ---\n" + sw.toString();
 
-                val logDir = context.getExternalFilesDir(null) ?: context.filesDir
-                val logFile = File(logDir, "NPATCH_CRASH.txt")
+                File logDir = ctx.getExternalFilesDir(null) != null ? ctx.getExternalFilesDir(null) : ctx.getFilesDir();
+                File logFile = new File(logDir, "NPATCH_CRASH.txt");
                 
-                logFile.writeBytes(report.toByteArray())
-            } catch (e: Exception) {}
-            originalHandler?.uncaughtException(thread, throwable)
-        }
+                try (java.io.FileOutputStream fos = new java.io.FileOutputStream(logFile)) {
+                    fos.write(report.getBytes());
+                }
+            } catch (Exception e) {}
+            if (originalHandler != null) {
+                originalHandler.uncaughtException(thread, throwable);
+            }
+        });
     }
 }
