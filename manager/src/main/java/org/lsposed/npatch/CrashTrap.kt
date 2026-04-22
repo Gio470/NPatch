19e3b27e8080bd33aa72e8149367830cff55de45
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-04-22 21:45:24 +0700

    Update CrashTrap.kt

--- manager/src/main/java/org/lsposed/npatch/CrashTrap.kt
+++ manager/src/main/java/org/lsposed/npatch/CrashTrap.kt
@@ -5,32 +5,37 @@ import android.os.Build;
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.io.FileOutputStream;
 
 public class CrashTrap {
     public static void start(Context ctx) {
         if (ctx == null) return;
         final Thread.UncaughtExceptionHandler originalHandler = Thread.getDefaultUncaughtExceptionHandler();
 
-        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
-            try {
-                StringWriter sw = new StringWriter();
-                throwable.printStackTrace(new PrintWriter(sw));
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread thread, Throwable throwable) {
+                try {
+                    StringWriter sw = new StringWriter();
+                    throwable.printStackTrace(new PrintWriter(sw));
 
-                String report = "--- NPATCH CRASH REPORT ---\n" +
-                             "Device: " + Build.MODEL + "\n" +
-                             "Android: " + Build.VERSION.RELEASE + "\n" +
-                             "Reason: " + throwable.getMessage() + "\n\n" +
-                             "--- STACK TRACE ---\n" + sw.toString();
+                    String report = "--- NPATCH CRASH REPORT ---\n" +
+                                 "Device: " + Build.MODEL + "\n" +
+                                 "Android: " + Build.VERSION.RELEASE + "\n" +
+                                 "Reason: " + throwable.getMessage() + "\n\n" +
+                                 "--- STACK TRACE ---\n" + sw.toString();
 
-                File logDir = ctx.getExternalFilesDir(null) != null ? ctx.getExternalFilesDir(null) : ctx.getFilesDir();
-                File logFile = new File(logDir, "NPATCH_CRASH.txt");
+                    File logDir = ctx.getExternalFilesDir(null) != null ? ctx.getExternalFilesDir(null) : ctx.getFilesDir();
+                    File logFile = new File(logDir, "NPATCH_CRASH.txt");
+                    
+                    try (FileOutputStream fos = new FileOutputStream(logFile)) {
+                        fos.write(report.getBytes());
+                    }
+                } catch (Exception e) {}
                 
-                try (java.io.FileOutputStream fos = new java.io.FileOutputStream(logFile)) {
-                    fos.write(report.getBytes());
+                if (originalHandler != null) {
+                    originalHandler.uncaughtException(thread, throwable);
                 }
-            } catch (Exception e) {}
-            if (originalHandler != null) {
-                originalHandler.uncaughtException(thread, throwable);
             }
         });
     }
