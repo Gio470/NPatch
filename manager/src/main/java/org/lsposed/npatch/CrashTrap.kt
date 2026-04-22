f5cbb0306c76524f0f8590320e93f71713b81968
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-04-22 21:56:40 +0700

    Update CrashTrap.kt

--- manager/src/main/java/org/lsposed/npatch/CrashTrap.kt
+++ manager/src/main/java/org/lsposed/npatch/CrashTrap.kt
@@ -1,42 +1,35 @@
-package org.lsposed.npatch;
+package org.lsposed.npatch
 
-import android.content.Context;
-import android.os.Build;
-import java.io.File;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.io.FileOutputStream;
+import android.content.Context
+import android.os.Build
+import java.io.File
+import java.io.PrintWriter
+import java.io.StringWriter
 
-public class CrashTrap {
-    public static void start(Context ctx) {
-        if (ctx == null) return;
-        final Thread.UncaughtExceptionHandler originalHandler = Thread.getDefaultUncaughtExceptionHandler();
-
-        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-            @Override
-            public void uncaughtException(Thread thread, Throwable throwable) {
-                try {
-                    StringWriter sw = new StringWriter();
-                    throwable.printStackTrace(new PrintWriter(sw));
-
-                    String report = "--- NPATCH CRASH REPORT ---\n" +
-                                 "Device: " + Build.MODEL + "\n" +
-                                 "Android: " + Build.VERSION.RELEASE + "\n" +
-                                 "Reason: " + throwable.getMessage() + "\n\n" +
-                                 "--- STACK TRACE ---\n" + sw.toString();
-
-                    File logDir = ctx.getExternalFilesDir(null) != null ? ctx.getExternalFilesDir(null) : ctx.getFilesDir();
-                    File logFile = new File(logDir, "NPATCH_CRASH.txt");
-                    
-                    try (FileOutputStream fos = new FileOutputStream(logFile)) {
-                        fos.write(report.getBytes());
-                    }
-                } catch (Exception e) {}
+object CrashTrap {
+    @JvmStatic
+    fun start(ctx: Context?) {
+        val context = ctx ?: return
+        val originalHandler = Thread.getDefaultUncaughtExceptionHandler()
+        
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            try {
+                val sw = StringWriter()
+                throwable.printStackTrace(PrintWriter(sw))
                 
-                if (originalHandler != null) {
-                    originalHandler.uncaughtException(thread, throwable);
-                }
+                val report = "--- NPATCH CRASH REPORT ---\n" +
+                             "Device: " + Build.MODEL + "\n" +
+                             "Android: " + Build.VERSION.RELEASE + "\n" +
+                             "Reason: " + throwable.message + "\n\n" +
+                             "--- STACK TRACE ---\n" + sw.toString()
+
+                val logDir = context.getExternalFilesDir(null) ?: context.filesDir
+                val logFile = File(logDir, "NPATCH_CRASH.txt")
+                
+                logFile.writeBytes(report.toByteArray())
+            } catch (e: Exception) {
             }
-        });
+            originalHandler?.uncaughtException(thread, throwable)
+        }
     }
 }
