9e789db52ee6faad0caf3234baa7cf618f86bf75
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-04-23 21:53:25 +0700

    Update LSPLoader.java

--- patch-loader/src/main/java/org/lsposed/npatch/loader/LSPLoader.java
+++ patch-loader/src/main/java/org/lsposed/npatch/loader/LSPLoader.java
@@ -31,7 +31,8 @@ public class LSPLoader {
             // Use reflection to avoid direct type reference to android.content.res.XResources
             // which fails class resolution on Android 16+ due to strict boot classloader
             // namespace delegation for the android.content.res.* package.
-            Class<?> xResourcesClass = Class.forName("android.content.res.XResources", false, ClassLoader.getSystemClassLoader());
+            ClassLoader cl = LSPLoader.class.getClassLoader();
+            Class<?> xResourcesClass = cl.loadClass("android.content.res.XResources");
             Method setMethod = xResourcesClass.getMethod("setPackageNameForResDir", String.class, String.class);
             setMethod.invoke(null, packageName, resDir);
         } catch (Throwable e) {
