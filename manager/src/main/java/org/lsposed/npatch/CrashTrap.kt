c16eb5ff1e2b1ad605c602154465dae52bd56e4a
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-04-23 21:25:29 +0700

    Update CrashTrap.kt

--- manager/src/main/java/org/lsposed/npatch/CrashTrap.kt
+++ manager/src/main/java/org/lsposed/npatch/CrashTrap.kt
@@ -11,24 +11,35 @@ object CrashTrap {
     fun start(ctx: Context?) {
         val context = ctx ?: return
         val originalHandler = Thread.getDefaultUncaughtExceptionHandler()
-        
+
         Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
             try {
                 val sw = StringWriter()
                 throwable.printStackTrace(PrintWriter(sw))
-                
-                val report = "--- NPATCH CRASH REPORT ---\n" +
-                             "Device: " + Build.MODEL + "\n" +
-                             "Android: " + Build.VERSION.RELEASE + "\n" +
-                             "Reason: " + throwable.message + "\n\n" +
-                             "--- STACK TRACE ---\n" + sw.toString()
+
+                val report = buildString {
+                    appendLine("--- NPATCH CRASH REPORT ---")
+                    appendLine("Device: ${Build.MODEL}")
+                    appendLine("Android: ${Build.VERSION.RELEASE}")
+                    appendLine("Thread: ${thread.name}")
+                    appendLine("Reason: ${throwable.message}")
+                    appendLine()
+                    appendLine("--- STACK TRACE ---")
+                    appendLine(sw.toString())
+                    appendLine()
+                    appendLine("--- CAUSED BY ---")
+                    var cause = throwable.cause
+                    while (cause != null) {
+                        appendLine("Caused by: ${cause.javaClass.name}: ${cause.message}")
+                        cause.stackTrace.take(5).forEach { appendLine("\tat $it") }
+                        cause = cause.cause
+                    }
+                }
 
                 val logDir = context.getExternalFilesDir(null) ?: context.filesDir
                 val logFile = File(logDir, "NPATCH_CRASH.txt")
-                
-                logFile.writeBytes(report.toByteArray())
-            } catch (e: Exception) {
-            }
+                logFile.writeText(report)
+            } catch (e: Exception) {}
             originalHandler?.uncaughtException(thread, throwable)
         }
     }
