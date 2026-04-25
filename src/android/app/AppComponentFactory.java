a7f3810146510dc89f8e01055370b2853e28474d
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-04-25 21:20:37 +0700

    Update AppComponentFactory.java

--- src/android/app/AppComponentFactory.java
+++ src/android/app/AppComponentFactory.java
@@ -1,58 +1,48 @@
 package android.app;
 
-import android.annotation.SuppressLint;
+import android.annotation.NonNull;
+import android.annotation.Nullable;
 import android.content.BroadcastReceiver;
 import android.content.ContentProvider;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
-import android.os.Build;
-import android.util.Log;
 
-import org.lsposed.npatch.metaloader.LSPAppComponentFactoryStub;
+public class AppComponentFactory {
 
-@SuppressLint("UnsafeDynamicallyLoadedCode")
-public class AppComponentFactory extends Application {
-    private static final String TAG = "AppComponentFactory";
-    private static boolean initialized = false;
-
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        if (!initialized) {
-            initialized = true;
-            LSPAppComponentFactoryStub.bootstrap();
-        }
-    }
-
-    public ClassLoader instantiateClassLoader(ClassLoader cl, ApplicationInfo appInfo) {
+    public @NonNull ClassLoader instantiateClassLoader(@NonNull ClassLoader cl,
+            @NonNull ApplicationInfo aInfo) {
         return cl;
     }
 
-    public Application instantiateApplication(ClassLoader cl, String className)
+    public @NonNull Application instantiateApplication(@NonNull ClassLoader cl,
+            @NonNull String className)
             throws InstantiationException, IllegalAccessException, ClassNotFoundException {
         return (Application) cl.loadClass(className).newInstance();
     }
 
-    public Activity instantiateActivity(ClassLoader cl, String className, Intent intent)
+    public @NonNull Activity instantiateActivity(@NonNull ClassLoader cl, @NonNull String className,
+            @Nullable Intent intent)
             throws InstantiationException, IllegalAccessException, ClassNotFoundException {
         return (Activity) cl.loadClass(className).newInstance();
     }
 
-    public BroadcastReceiver instantiateReceiver(ClassLoader cl, String className, Intent intent)
+    public @NonNull BroadcastReceiver instantiateReceiver(@NonNull ClassLoader cl,
+            @NonNull String className, @Nullable Intent intent)
             throws InstantiationException, IllegalAccessException, ClassNotFoundException {
         return (BroadcastReceiver) cl.loadClass(className).newInstance();
     }
 
-    public Service instantiateService(ClassLoader cl, String className, Intent intent)
+    public @NonNull Service instantiateService(@NonNull ClassLoader cl,
+            @NonNull String className, @Nullable Intent intent)
             throws InstantiationException, IllegalAccessException, ClassNotFoundException {
         return (Service) cl.loadClass(className).newInstance();
     }
 
-    public ContentProvider instantiateProvider(ClassLoader cl, String className)
+    public @NonNull ContentProvider instantiateProvider(@NonNull ClassLoader cl,
+            @NonNull String className)
             throws InstantiationException, IllegalAccessException, ClassNotFoundException {
         return (ContentProvider) cl.loadClass(className).newInstance();
     }
 
     public static final AppComponentFactory DEFAULT = new AppComponentFactory();
-          }
+    }
