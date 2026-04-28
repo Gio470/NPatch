543ad3aa954ae0408cdd41d8d670276e6b309229
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-04-28 11:59:54 +0700

    Update AppComponentFactory.java

--- src/android/app/AppComponentFactory.java
+++ src/android/app/AppComponentFactory.java
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
@@ -12,81 +11,55 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 public class AppComponentFactory extends ContentProvider {
-
     public static final AppComponentFactory DEFAULT = new AppComponentFactory();
 
     @Override
     public boolean onCreate() {
         if (Build.VERSION.SDK_INT < 28) {
-            bootstrap();
+            try {
+                Class<?> atc = Class.forName("android.app.ActivityThread");
+                Method catm = atc.getDeclaredMethod("currentActivityThread");
+                catm.setAccessible(true);
+                Object at = catm.invoke(null);
+                Field instField = atc.getDeclaredField("mInstrumentation");
+                instField.setAccessible(true);
+                Instrumentation old = (Instrumentation) instField.get(at);
+                instField.set(at, new ProxyInst(this, old));
+            } catch (Throwable ignored) {}
         }
         return true;
     }
 
-    private void bootstrap() {
-        try {
-            Class<?> atc = Class.forName("android.app.ActivityThread");
-            Method catm = atc.getDeclaredMethod("currentActivityThread");
-            catm.setAccessible(true);
-            Object at = catm.invoke(null);
-
-            Field instField = atc.getDeclaredField("mInstrumentation");
-            instField.setAccessible(true);
-            Instrumentation oldInst = (Instrumentation) instField.get(at);
-            
-            instField.set(at, new ProxyInst(this, oldInst));
-        } catch (Throwable ignored) {
-        }
-    }
-
-    public Application instantiateApplication(ClassLoader cl, String className)
-            throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+    public Application instantiateApplication(ClassLoader cl, String className) throws Exception {
         return (Application) cl.loadClass(className).newInstance();
     }
 
-    public Activity instantiateActivity(ClassLoader cl, String className, Intent intent)
-            throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+    public Activity instantiateActivity(ClassLoader cl, String className, Intent intent) throws Exception {
         return (Activity) cl.loadClass(className).newInstance();
     }
 
-    public BroadcastReceiver instantiateReceiver(ClassLoader cl, String className, Intent intent)
-            throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+    public BroadcastReceiver instantiateReceiver(ClassLoader cl, String className, Intent intent) throws Exception {
         return (BroadcastReceiver) cl.loadClass(className).newInstance();
     }
 
-    public Service instantiateService(ClassLoader cl, String className, Intent intent)
-            throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+    public Service instantiateService(ClassLoader cl, String className, Intent intent) throws Exception {
         return (Service) cl.loadClass(className).newInstance();
     }
 
-    public ContentProvider instantiateProvider(ClassLoader cl, String className)
-            throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+    public ContentProvider instantiateProvider(ClassLoader cl, String className) throws Exception {
         return (ContentProvider) cl.loadClass(className).newInstance();
-    }
-
-    public ClassLoader instantiateClassLoader(ClassLoader cl, ApplicationInfo aInfo) {
-        return cl;
     }
 
     private static class ProxyInst extends Instrumentation {
         private final AppComponentFactory f;
         private final Instrumentation o;
-
-        ProxyInst(AppComponentFactory factory, Instrumentation old) {
-            f = factory;
-            o = old;
-        }
-
+        ProxyInst(AppComponentFactory factory, Instrumentation old) { f = factory; o = old; }
         @Override
-        public Activity newActivity(ClassLoader cl, String className, Intent intent)
-                throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+        public Activity newActivity(ClassLoader cl, String className, Intent intent) throws Exception {
             return f.instantiateActivity(cl, className, intent);
         }
-
         @Override
-        public void callApplicationOnCreate(Application app) {
-            o.callApplicationOnCreate(app);
-        }
+        public void callApplicationOnCreate(Application app) { o.callApplicationOnCreate(app); }
     }
 
     @Override public Cursor query(Uri u, String[] s, String r, String[] a, String o) { return null; }
@@ -95,4 +68,5 @@ public class AppComponentFactory extends ContentProvider {
     @Override public int delete(Uri u, String s, String[] a) { return 0; }
     @Override public int update(Uri u, ContentValues v, String s, String[] a) { return 0; }
 }
+
 
