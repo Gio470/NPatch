0d3b1b823891eb31566521ffa4ca0703e54f357e
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-04-29 18:19:19 +0700

    Update LSPAppComponentFactoryStub.java

--- meta-loader/src/main/java/org/lsposed/npatch/metaloader/LSPAppComponentFactoryStub.java
+++ meta-loader/src/main/java/org/lsposed/npatch/metaloader/LSPAppComponentFactoryStub.java
@@ -2,7 +2,7 @@ package org.lsposed.npatch.metaloader;
 
 import android.annotation.SuppressLint;
 import android.app.ActivityThread;
-import android.app.AppComponentFactory;
+import android.app.AppComponentFactoryStub;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.IPackageManager;
 import android.os.Build;
@@ -27,7 +27,7 @@ import java.util.Objects;
 import java.util.zip.ZipFile;
 
 @SuppressLint("UnsafeDynamicallyLoadedCode")
-public class LSPAppComponentFactoryStub extends AppComponentFactory {
+public class LSPAppComponentFactoryStub extends AppComponentFactoryStub {
 
     private static final String TAG = "NPatch-MetaLoader";
     private static final Map<String, String> archToLib = new HashMap<String, String>(4);
