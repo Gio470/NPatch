742dbec58c6196fec8f00af62cdbb922c1a0e93a
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-04-29 12:29:48 +0700

    Update AppComponentFactoryStub.java

--- src/android/app/AppComponentFactoryStub.java
+++ src/android/app/AppComponentFactoryStub.java
@@ -3,20 +3,15 @@ package android.app;
 import android.content.BroadcastReceiver;
 import android.content.ContentProvider;
 import android.content.ContentValues;
-import android.content.Intent;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.WeakHashMap;
 
 public class AppComponentFactory {
-
-    private static final WeakHashMap<android.content.pm.ApplicationInfo, String> sStorage = new WeakHashMap<>();
-
     public Application instantiateApplication(ClassLoader cl, String className) throws Exception {
         return (Application) cl.loadClass(className).newInstance();
     }
@@ -38,53 +33,36 @@ public class AppComponentFactory {
     }
 
     public static class BackportProvider extends ContentProvider {
-
         @Override
         public boolean onCreate() {
             if (Build.VERSION.SDK_INT < 28) {
                 try {
-                    setup();
+                    String fName = parse();
+                    if (fName != null) {
+                        Class<?> atc = Class.forName("android.app.ActivityThread");
+                        Object at = atc.getDeclaredMethod("currentActivityThread").invoke(null);
+                        Field ifld = atc.getDeclaredField("mInstrumentation");
+                        ifld.setAccessible(true);
+                        Instrumentation base = (Instrumentation) ifld.get(at);
+                        AppComponentFactory factory = (AppComponentFactory) getContext().getClassLoader().loadClass(fName).newInstance();
+                        ifld.set(at, new ProxyInst(base, factory));
+                    }
                 } catch (Throwable ignored) {}
             }
             return true;
         }
 
-        private void setup() throws Exception {
-            android.content.Context ctx = getContext();
-            String factory = parseFactory(ctx);
-            if (factory == null) return;
-
-            sStorage.put(ctx.getApplicationInfo(), factory);
-
-            Class<?> atClass = Class.forName("android.app.ActivityThread");
-            Method m = atClass.getDeclaredMethod("currentActivityThread");
-            m.setAccessible(true);
-            Object at = m.invoke(null);
-
-            Field f = atClass.getDeclaredField("mInstrumentation");
-            f.setAccessible(true);
-            Instrumentation base = (Instrumentation) f.get(at);
-
-            if (!(base instanceof ProxyInst)) {
-                AppComponentFactoryStub stub = (AppComponentFactoryStub) ctx.getClassLoader()
-                    .loadClass(factory).newInstance();
-                f.set(at, new ProxyInst(base, stub));
-            }
-        }
-
-        private String parseFactory(android.content.Context ctx) {
+        private String parse() {
             try {
-                android.content.res.XmlResourceParser p = ctx.getAssets().openXmlResourceParser("AndroidManifest.xml");
-                int type;
-                while ((type = p.next()) != android.content.res.XmlResourceParser.END_DOCUMENT) {
-                    if (type == android.content.res.XmlResourceParser.START_TAG && "application".equals(p.getName())) {
+                android.content.res.XmlResourceParser p = getContext().getAssets().openXmlResourceParser("AndroidManifest.xml");
+                int t;
+                while ((t = p.next()) != 1) {
+                    if (t == 2 && "application".equals(p.getName())) {
                         for (int i = 0; i < p.getAttributeCount(); i++) {
-                            if ("appComponentFactory".equals(p.getAttributeName(i)))
-                                return p.getAttributeValue(i);
+                            if ("appComponentFactory".equals(p.getAttributeName(i))) return p.getAttributeValue(i);
                         }
                     }
                 }
-                p.close();
             } catch (Throwable ignored) {}
             return null;
         }
@@ -98,32 +76,33 @@ public class AppComponentFactory {
 
     private static class ProxyInst extends Instrumentation {
         private final Instrumentation b;
-        private final AppComponentFactoryStub f;
-
-        ProxyInst(Instrumentation base, AppComponentFactoryStub stub) { b = base; f = stub; }
+        private final AppComponentFactory f;
+        ProxyInst(Instrumentation base, AppComponentFactory factory) { b = base; f = factory; }
 
         @Override
         public Activity newActivity(ClassLoader cl, String className, Intent intent) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
-            try { return f.instantiateActivity(cl, className, intent); }
-            catch (Exception e) { throw new InstantiationException(e.getMessage()); }
+            try { return f.instantiateActivity(cl, className, intent); } catch (Exception e) { throw new InstantiationException(e.getMessage()); }
         }
 
         @Override
         public Application newApplication(ClassLoader cl, String className, Context ctx) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
             try {
-                Application app = f.instantiateApplication(cl, className);                
+                Application app = f.instantiateApplication(cl, className);
+                Method m = Application.class.getDeclaredMethod("attach", Context.class);
+                m.setAccessible(true);
+                m.invoke(app, ctx);
                 return app;
             } catch (Exception e) { throw new InstantiationException(e.getMessage()); }
         }
 
-        @Override public void callApplicationOnCreate(Application app) { b.callApplicationOnCreate(app); }
-        @Override public void callActivityOnCreate(Activity a, android.os.Bundle ic) { b.callActivityOnCreate(a, ic); }
+        @Override public void callApplicationOnCreate(Application a) { b.callApplicationOnCreate(a); }
+        @Override public void callActivityOnCreate(Activity a, android.os.Bundle i) { b.callActivityOnCreate(a, i); }
         @Override public void callActivityOnDestroy(Activity a) { b.callActivityOnDestroy(a); }
         @Override public void callActivityOnPause(Activity a) { b.callActivityOnPause(a); }
         @Override public void callActivityOnResume(Activity a) { b.callActivityOnResume(a); }
         @Override public void callActivityOnStart(Activity a) { b.callActivityOnStart(a); }
         @Override public void callActivityOnStop(Activity a) { b.callActivityOnStop(a); }
     }
-}
+            }
 
 
